67c541c89bd9e133c38835efe9966fa7ecaeab11
Larson <troylars@amazon.com>
2017-05-08 17:41:32 -0400

    Added LICENSE and README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="ppt-time-referee",
-    version='0.7.3',
+    version='0.7.4',
     packages=['time_referee'],
     py_modules=['referee'],
     description='Simple time-awareness tool for PowerPoint presentations',
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         'Click','arrow','python-pptx'
     ],
+    data_files=[('', ['LICENSE'])]
     entry_points='''
         [console_scripts]
         ppt-ref=referee:cli
