d5a92d7aca127729aac4966c17c90bfa51faf60f
Troy Larson <troylar@gmail.com>
2017-05-08 16:50:24 -0400

    Fixed setup package

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name="ppt-time-referee",
-    version='0.1',
+    version='0.7.3',
+    packages=['time_referee'],
     py_modules=['referee'],
+    description='Simple time-awareness tool for PowerPoint presentations',
+    author="Troy Larson",
+    author_email='troylar@gmail.com',
     install_requires=[
-        'Click',
+        'Click','arrow','python-pptx'
     ],
     entry_points='''
         [console_scripts]
